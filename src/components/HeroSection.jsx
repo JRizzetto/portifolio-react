c7506ec602f89e487437b81fd66a501e8c7dcb9e
jeffersonrizzetto@gmail.com <jeffersonrizzetto@gmail.com>
2025-05-30 11:24:29 -0300

    Ajustando foto HeroSection

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -7,6 +7,9 @@ export const HeroSection = () => {
       className="relative min-h-screen flex flex-col items-center justify-center px-4"
     >
       <div className="container max-w-4xl mx-auto text-center z-10">
+        <div className="flex justify-center items-center">
+          <img src="/projects/minha-foto-img.png" alt='Minha-foto'/>
+        </div>
         <div className="space-y-6">
           <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
             <span className="opacity-0 animate-fade-in">Olá, Eu sou </span>
