95c40e45a94ea2463b63b34e8375b1565240e425
JRizzetto <jeffersonrizzetto@gmail.com>
2026-01-27 09:35:42 -0300

    Fix connection between backend and render - 2

--- backend/src/config/mail.config.js
+++ backend/src/config/mail.config.js
@@ -1,19 +1,11 @@
-import nodemailer from "nodemailer";
-import { getMailConfig } from "../config/mail.config.js";
-
-const transporter = nodemailer.createTransport(getMailConfig());
-
-export async function sendContactMail({ name, email, message }) {
-  await transporter.sendMail({
-    from: `"Portfolio Contact" <${process.env.MAIL_USER}>`,
-    to: process.env.MAIL_USER,
-    subject: "New Contact Message",
-    text: `
-Name: ${name}
-Email: ${email}
-
-Message:
-${message}
-    `,
-  });
+export function getMailConfig() {
+  return {
+    host: process.env.MAIL_HOST,
+    port: Number(process.env.MAIL_PORT),
+    secure: false,
+    auth: {
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASS,
+    },
+  };
 }
