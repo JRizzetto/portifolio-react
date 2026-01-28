880dfc2da04404065554363d1ea5a02d84c61f06
JRizzetto <jeffersonrizzetto@gmail.com>
2026-01-28 18:53:01 -0300

    Tradução PT-BR para EN-US - Arrumando Bug case sensitive

--- frontend/src/i18n/index.js
+++ frontend/src/i18n/index.js
@@ -2,7 +2,7 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
 import en from "../locales/en/translation.json";
-import ptBR from "../locales/pt-BR/translation.json";
+import ptBR from "../locales/pt-br/translation.json";
 
 const savedLanguage = localStorage.getItem("language") || "pt-BR";
 
